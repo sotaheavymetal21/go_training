e02847e8b523363eb834ee0a962209c07b0ee808
Apple <apple@Sotanomakku.local>
2022-11-26 14:52:14 +0900

    helloworld実行

---
+++ AwesomeProject/lesson.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+func init() {
+	fmt.Println("Init!")
+}
+*/
+
+func bazz() {
+	fmt.Println("Bazz")
+}
+
+func main() {
+	// bazz()
+	fmt.Println("HelloWorld", "test")
+}
