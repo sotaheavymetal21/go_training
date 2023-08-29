3fe738402ca0137f08e63cdd09dd01b3bf2a765d
sotaheavymetal21 <euro246seeker@gmail.com>
2023-08-29 23:19:50 +0900

    string

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var (
-		u8  uint8     = 255
-		i8  int8      = 127
-		f32 float32   = 0.2
-		c64 complex64 = -5 + 12i
-	)
-	fmt.Println(u8, i8, f32, c64)
-	fmt.Printf("type=%T value=%v", u8, u8)
+	fmt.Println("Hello Go")
+	fmt.Println(string("Hello Go"[0]))
+	var s string = "Hello Go"
 
-	x := 0
-	fmt.Println(x)
-	// x = x + 1
-	x++
-	// x = x - 1
-	x--
-
-	fmt.Println(1 << 0) // 0001 0001: 1
-	fmt.Println(1 << 1) // 0001 0010: 2
-	fmt.Println(1 << 2) // 0001 0100: 4
-	fmt.Println(1 << 3) // 0001 1000: 8
+	s = strings.Replace(s, "H", "X", 1)
+	fmt.Println(s)
+	fmt.Println(strings.Contains(s, "Go"))
+	fmt.Println("\"")
+	fmt.Println('"')
 }
