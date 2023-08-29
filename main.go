ef4911c4e715da9b9f34cd64b424368e776c77c8
sotaheavymetal21 <euro246seeker@gmail.com>
2023-08-29 22:49:23 +0900

    型指定

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"time"
 )
 
+// i5 := 500
+var i5 int = 100
+
+func outer() {
+	var s4 string = "outer"
+	fmt.Println(s4)
+}
+
 func main() {
-	engine := gin.Default()
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
-	engine.Run(":3000")
+	fmt.Println("Hello World!")
+	fmt.Println(time.Now())
+	var i int = 100
+	fmt.Println(i)
+
+	var str string = "Hello Go"
+	fmt.Println(str)
+
+	var (
+		i2 int
+		s2 string
+	)
+	i2 = 300
+	s2 = "Go"
+	fmt.Println(i2, s2)
+
+	i = 150
+	fmt.Println(i)
+
+	// 暗黙的な定義、型推論
+	// 変数名 := 値
+
+	i4 := 400
+	fmt.Println(i4)
+
+	fmt.Println(i5)
+
+	outer()
+
 }
